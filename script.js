7d569bafcdf79b29cc7c60797df0f6f1bf89668c
Gisbyy <grantsbrowne@gmail.com>
2021-02-02 16:27:42 +0000

    Added RNG for arrays

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // Variables for weather message generation
 const randomMessage = {
     location: ['Glasgow', 'Berlin', 'Paris', 'Madrid', 'Copenhagen', 'New York', 'Tokyo', 'Sydney', 'Moscow', 'Athens'],
-    weatherType: ['Raining', 'Snowing', 'Sunny', 'Overcast', 'Sleeting', 'Freezing'],
-    activity: ['Sleep', 'Cycle', 'Run', 'Swim', 'Eat']
+    weather: ['Raining', 'Snowing', 'Sunny', 'Overcast', 'Sleeting', 'Freezing'],
+    activity: ['Sleep', 'Cycle', 'Run', 'Swim', 'Eat'],
+    generateMessage(){
+        let randLoc = Math.floor(Math.random() * this.location.length);
+        let randWeather = Math.floor(Math.random() * this.weather.length);
+        let randActivity = Math.floor(Math.random() * this.activity.length);
+        
+    }
 }
+
+
+
 
