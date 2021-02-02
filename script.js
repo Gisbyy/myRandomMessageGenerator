c9c6bc01681de40577fbf83135246c6ac4aa675d
Gisbyy <grantsbrowne@gmail.com>
2021-02-02 17:23:48 +0000

    Reconfig of rng variables and final message

--- script.js
+++ script.js
@@ -5,16 +5,14 @@ generateMessage = () => {
     const location = ['Glasgow', 'Berlin', 'Paris', 'Madrid', 'Copenhagen', 'New York', 'Tokyo', 'Sydney', 'Moscow', 'Athens'];
     const activity = ['sleep', 'cycle', 'run', 'swim', 'eat'];
 
-    // Random number generator for each array
-    let randWeather = Math.floor(Math.random() * weather.length);
-    let randLoc = Math.floor(Math.random() * location.length);
-    let randActivity = Math.floor(Math.random() * activity.length);
+    // Random generator for each array, placed in variable
+    let randWeather = weather[Math.floor(Math.random() * weather.length)];
+    let randLoc = location[Math.floor(Math.random() * location.length)];
+    let randActivity = activity[Math.floor(Math.random() * activity.length)];
     
     // Final message logged to console
-    console.log('When it is ' + (weather[randWeather]) + ' in ' + (location[randLoc]) + ', I like to ' + (activity[randActivity]));
+    console.log('When it is ' + randWeather + ' in ' + randLoc + ', I like to ' + randActivity + '.');
 }
    
 generateMessage();
-
-
 
