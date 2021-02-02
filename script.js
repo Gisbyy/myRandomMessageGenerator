c16016ee1989ee7a18b1f8f8a4cf9e6961ca2891
Gisbyy <grantsbrowne@gmail.com>
2021-02-02 16:16:37 +0000

    Added randomMessage Arrays

---
+++ script.js
@@ -0,0 +1,7 @@
+// Variables for weather message generation
+const randomMessage = {
+    location: ['Glasgow', 'Berlin', 'Paris', 'Madrid', 'Copenhagen', 'New York', 'Tokyo', 'Sydney', 'Moscow', 'Athens'],
+    weatherType: ['Raining', 'Snowing', 'Sunny', 'Overcast', 'Sleeting', 'Freezing'],
+    activity: ['Sleep', 'Cycle', 'Run', 'Swim', 'Eat']
+}
+
